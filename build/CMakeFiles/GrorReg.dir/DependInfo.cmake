2b7fbd8f851ff613e44a7ae350ca97c83d2b2720
pw-pq <15773158911@163.com>
2024-03-20 16:28:07 +0800

    1

---
+++ build/CMakeFiles/GrorReg.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pw/project/Registration/GROR/src/gror_pre.cpp" "/home/pw/project/Registration/GROR/build/CMakeFiles/GrorReg.dir/src/gror_pre.cpp.o"
+  "/home/pw/project/Registration/GROR/src/main.cpp" "/home/pw/project/Registration/GROR/build/CMakeFiles/GrorReg.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_FILESYSTEM_DYN_LINK"
+  "BOOST_IOSTREAMS_DYN_LINK"
+  "BOOST_SERIALIZATION_DYN_LINK"
+  "BOOST_SYSTEM_DYN_LINK"
+  "EIGEN_HAS_CXX17_OVERALIGN=0"
+  "PCL_NO_PRECOMPILE"
+  "_CRT_SECURE_NO_WARNINGS"
+  "_EXPORTING"
+  "kiss_fft_scalar=double"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/eigen3"
+  "/usr/local/include/pcl-1.14"
+  "/usr/local/include/vtk-9.3"
+  "/usr/local/include/vtk-9.3/vtknlohmannjson/include"
+  "/usr/local/include/vtk-9.3/vtkfreetype/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/GrorReg.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GrorReg.dir/src/gror_pre.cpp.o"
+  "CMakeFiles/GrorReg.dir/src/main.cpp.o"
+  "GrorReg"
+  "GrorReg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GrorReg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
